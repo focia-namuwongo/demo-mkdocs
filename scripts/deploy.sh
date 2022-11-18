20d40ed2c315bc799bc71385fa7aacd3c74fbbfa
ntno <ntno@users.noreply.github.com>
2022-11-17 19:45:01 -0500

    bug fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,5 +8,5 @@ source ./scripts/build.sh $1 $2
 
 ## deploy
 eval "$(buildenv -e $1 -d $2)" && \
-export WEBSITE_S3_ID=`aws ssm get-parameters --name "$WEBSITE_S3_ID" | jq -r .Parameters[0].Value` && \
+export WEBSITE_S3_ID=`aws ssm get-parameters --name "$WEBSITE_S3_ID_SSM_PATH" | jq -r .Parameters[0].Value` && \
 aws s3 sync --size-only --sse AES256 --acl public-read ./site/ "s3://$WEBSITE_S3_ID"
