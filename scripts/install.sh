b148c6e98253f4497d67a15b58118210d7b2850e
ntno <ntno@users.noreply.github.com>
2022-12-15 16:25:28 -0500

    troubleshooting

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,11 +5,11 @@ sudo apt-get install -y gettext groff
 
 
 export BUILDENV_RELEASE=0.5.7
-mkdir /tmp
+
 cd /usr/local/bin && \
 wget -O /tmp/buildenv.tar.gz https://github.com/Comcast/Buildenv-Tool/releases/download/${BUILDENV_RELEASE}/buildenv-linux_amd64-${BUILDENV_RELEASE}.tar.gz && \
 cd /tmp && \
-tar xzvf /tmp/buildenv.tar.gz \
+tar xzvf /tmp/buildenv.tar.gz && \
 mv /tmp/buildenv /usr/local/bin/buildenv && \ 
 chown root:root /usr/local/bin/buildenv && \ 
 chmod 755 /usr/local/bin/buildenv && \
