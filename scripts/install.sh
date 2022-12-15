44f08d9d4d68e371afcdd4cdf2dede14f7004c88
ntno <ntno@users.noreply.github.com>
2022-12-15 16:27:41 -0500

    troubleshooting

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,7 +9,11 @@ export BUILDENV_RELEASE=0.5.7
 cd /usr/local/bin && \
 wget -O /tmp/buildenv.tar.gz https://github.com/Comcast/Buildenv-Tool/releases/download/${BUILDENV_RELEASE}/buildenv-linux_amd64-${BUILDENV_RELEASE}.tar.gz && \
 cd /tmp && \
-tar xzvf /tmp/buildenv.tar.gz && \
+tar xzvf /tmp/buildenv.tar.gz 
+
+ls -ltra /tmp
+mkdir -p /usr/local/bin/
+ls -ltra /usr/local/bin
 mv /tmp/buildenv /usr/local/bin/buildenv && \ 
 chown root:root /usr/local/bin/buildenv && \ 
 chmod 755 /usr/local/bin/buildenv && \
