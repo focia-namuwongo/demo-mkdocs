f6a70312810af7adef5c248eb3a041d76de95c42
ntno <ntno@users.noreply.github.com>
2022-12-15 16:34:55 -0500

    more sudo?

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,12 +6,10 @@ sudo apt-get install -y gettext groff
 
 export BUILDENV_RELEASE=0.5.7
 
-sudo cd /usr/local/bin && \
-wget -O /tmp/buildenv.tar.gz https://github.com/Comcast/Buildenv-Tool/releases/download/${BUILDENV_RELEASE}/buildenv-linux_amd64-${BUILDENV_RELEASE}.tar.gz && \
-cd /tmp && \
-tar xzvf /tmp/buildenv.tar.gz && \
-mkdir -p /usr/local/bin/ && \
-mv /tmp/buildenv /usr/local/bin/buildenv && \ 
-chown root:root /usr/local/bin/buildenv && \ 
-chmod 755 /usr/local/bin/buildenv && \
-rm -rf /tmp/buildenv*
+sudo -D /usr/local/bin wget -O /tmp/buildenv.tar.gz https://github.com/Comcast/Buildenv-Tool/releases/download/${BUILDENV_RELEASE}/buildenv-linux_amd64-${BUILDENV_RELEASE}.tar.gz && \
+sudo -D /tmp tar xzvf /tmp/buildenv.tar.gz -C /tmp && \
+sudo mkdir -p /usr/local/bin/ && \
+sudo mv /tmp/buildenv /usr/local/bin/buildenv && \ 
+sudo chown root:root /usr/local/bin/buildenv && \ 
+sudo chmod 755 /usr/local/bin/buildenv && \
+sudo rm -rf /tmp/buildenv*
